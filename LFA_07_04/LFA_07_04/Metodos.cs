e42f402dbfe1e6b1f56d25edebcbe21c0257aac6
Yurilucaslima17 <yurilucaslima17@gmail.com>
2020-04-14 15:18:14 -0300

    Add files via upload

---
+++ LFA_07_04/LFA_07_04/Metodos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFA_07_04
+{
+    class Metodos
+    {
+        public void Classificador(List<List<string>> regras, List<string> simbolos, List<string> terminais, string inicial )
+        {
+            List<string> n = new List<string>();
+
+            foreach (string item in simbolos)
+            {
+                if (!terminais.Contains(item))
+                {
+                    n.Add(item);
+                }
+            }
+
+            if (IsLinear())
+            {
+                Console.WriteLine("É Linear");
+            }
+            else if (IsLivre())
+            {
+
+            }
+            else if (IsSensivel())
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Grámatica Irrestrita");
+            }
+
+        }
+        //regras.Add("S", "S2");
+        public bool IsLinear()
+        {
+            return false;
+        }
+
+        public bool IsLivre()
+        {
+            return false;
+        }
+
+        public bool IsSensivel()
+        {
+            return false;
+        }
+    }
+}
---
+++ LFA_07_04/LFA_07_04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFA_07_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos metodos = new Metodos();
+
+            List<string> simbolos = new List<string>() { "0", "1", "2", "3", "S", "A" };
+            List<string> terminais = new List<string>() { "0", "1", "2", "3" };
+            List<List<string>> regras = new List<List<string>>() { 
+                new List<string>() { "S", "S3" },
+                new List<string>() { "S", "A" },
+                new List<string>() { "A", "1" }, 
+                new List<string>() { "A", "0" } 
+            };
+            string r = "S";
+
+            metodos.Classificador(regras,simbolos,terminais, r);
+
+
+
+        }
+
+        
+
+    }
+
+    
+
+}
